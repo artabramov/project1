1a1d9a4fde2607e7e3ff1e34d4cb14d5a4b7fdf9
Artem Abramov <notdepot@gmail.com>
2021-12-01 16:03:16 +0000

    little updates

--- python/app/app.py
+++ python/app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
     import sys
     output = '<h1>Hello, Flask! </h1>'
-    output += str(sys.version_info) + '<br>' + str(sys.path) + '<br>' + str(sys.modules.keys())
+    output += str(sys.version_info) + '<br>' + str(sys.path) + '<br>' + str(sys.modules.keys()) + '<br>'
+
+    for arg in request.args:
+        output += str(arg) + '<br>'
+    
     return output
 
