95fa1f1af1e100c06da7273ca8125c18ad4f0f4a
vagrant <vagrant@node1>
2021-12-02 08:15:10 +0000

    added rewrite rules to .htaccess

--- python/app/app.py
+++ python/app/app.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     import sys
-    output = '<h1>Hello, Flask! </h1>'
+    output = '<h1>Hello, Flask!</h1>'
     output += str(sys.version_info) + '<br>' + str(sys.path) + '<br>' + str(sys.modules.keys()) + '<br>'
-
-    for arg in request.args:
-        output += str(arg) + '<br>'
-    
+    output += str(request.args) + '<br>'
     return output
 
